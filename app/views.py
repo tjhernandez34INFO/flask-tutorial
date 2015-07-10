46853c1e78c7a7ef22840f45eea216d92de34c58
tjhernandez34INFO <travis.hernandez@infospace.com>
2015-07-10 11:36:35 -0700

    first commit

---
+++ app/views.py
@@ -0,0 +1,33 @@
+from flask import render_template, flash, redirect
+from app import app
+from .forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Travis'}
+    posts = [
+    	{
+    		'author': { 'nickname': 'John' },
+    		'body': 'Beautiful day in Bellevue!'
+    	},
+    	{
+    		'author': { 'nickname': 'Susan' },
+    		'body': 'Coding is cool!'
+    	}
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="%s", remember_me=%s' %
+				(form.openid.data, str(form.remember_me.data)))
+		return redirect('/index')
+	return render_template('login.html',
+							title='Sign In',
+							form=form)
